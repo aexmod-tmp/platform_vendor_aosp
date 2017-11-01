6bbd8c19cb7718202e9783945d46c30c7dd3a142
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:01:35 -0700

    aosp: Add TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY variable handling
    Change-Id: I6f1cbcd883e91c8c3724dbe53732c7e777e21657

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,60 +1,66 @@
 package android_aosp
 type Product_variables struct {
-    Needs_text_relocations struct {
-	Cppflags []string
-    }
+	Needs_text_relocations struct {
+		Cppflags []string
+	}
 
-    Uses_non_treble_camera struct {
-	Cflags []string
-    }
+	Uses_non_treble_camera struct {
+		Cflags []string
+	}
 
-    Has_legacy_camera_hal1 struct {
-	Cflags []string
-    }
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
 
-    Uses_media_extensions struct {
-	Cflags []string
-    }
+	Uses_media_extensions struct {
+		Cflags []string
+	}
 
-    BoardUsesQTIHardware struct {
-	Cflags []string
-	Cppflags []string
-    }
+	BoardUsesQTIHardware struct {
+		Cflags []string
+		Cppflags []string
+	}
 
-    BoardUsesQCOMHardware struct {
-	Cflags []string
-	Cppflags []string
-    }
+	BoardUsesQCOMHardware struct {
+		Cflags []string
+		Cppflags []string
+	}
 
-    TargetUsesQCOMBsp struct {
-	Cflags []string
-	Cppflags []string
-    }
+	TargetUsesQCOMBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
 
-    TargetUsesQCOMLegacyBsp struct {
-	Cflags []string
-	Cppflags []string
-    }
+	TargetUsesQCOMLegacyBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
 
-    BoardUsesLegacyAlsa struct {
-	Cflags []string
-	Cppflags []string
-    }
+	BoardUsesLegacyAlsa struct {
+		Cflags []string
+		Cppflags []string
+	}
 
-    Cant_reallocate_omx_buffers struct {
-	Cflags []string
-    }
+	Cant_reallocate_omx_buffers struct {
+		Cflags []string
+        }
+
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
 }
 
 type ProductVariables struct {
-    Needs_text_relocations  *bool `json:",omitempty"`
-    Uses_non_treble_camera  *bool `json:",omitempty"`
-    Has_legacy_camera_hal1  *bool `json:",omitempty"`
-    Uses_media_extensions   *bool `json:",omitempty"`
-    BoardUsesQTIHardware  *bool `json:",omitempty"`
-    BoardUsesQCOMHardware  *bool `json:",omitempty"`
-    TargetUsesQCOMBsp  *bool `json:",omitempty"`
-    TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
-    BoardUsesLegacyAlsa  *bool `json:",omitempty"`
-    Cant_reallocate_omx_buffers *bool `json:",omitempty"`
+	Needs_text_relocations  *bool `json:",omitempty"`
+	Uses_non_treble_camera  *bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Uses_media_extensions   *bool `json:",omitempty"`
+	BoardUsesQTIHardware  *bool `json:",omitempty"`
+	BoardUsesQCOMHardware  *bool `json:",omitempty"`
+	TargetUsesQCOMBsp  *bool `json:",omitempty"`
+	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
+	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
+	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
 }
