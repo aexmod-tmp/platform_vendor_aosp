77f698405bc35be677bcd075bb64a68c8ee3a470
Lane Shukhov <rl.shukhov@mail.ru>
2018-05-15 10:50:22 +0300

    Update 720p and 1080p bootanimations

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -21,9 +21,9 @@ ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),540)
      PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX-540.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX_TRANSFORMER-720.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEXMOD-720.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEX_TRANSFORMER-1080.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/AEXMOD-1080.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
      PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
